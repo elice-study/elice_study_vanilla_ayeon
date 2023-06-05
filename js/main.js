329be56b62d02708948ea2f86780dd74e0073675
joayeon <codinz.n@gmail.com>
2023-06-05 17:03:20 +0900

    :hammer:  add modal data
    모달창에 데이터를 받아와서 화면에 출력하였습니다.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 function movieLoad() {
-    return fetch('../db/movieData.json')
-        .then((Response) => Response.json())
-        .then((json) => json.movies);
+    // fetch 를 통해 해당하는 파일의 경로를 통해서 movieData.json 접근
+    return (
+        fetch('../db/movieData.json')
+            // then 을 통해 성공적으로 받아오면 response 획득 -> json 형태로 변환하기 위해 response.json
+            .then((Response) => Response.json())
+            // json 에 있는 movies 만 받아오기 위해 json.movies
+            .then((json) => json.movies)
+    );
 }
 
+// Main Movie-Img List Display
 movieLoad().then((movies) => {
-    console.log(movies);
+    // 제대로 받아와지는지 확인
+    // console.log(movies);
     movieList(movies);
 });
 
-// Main Movie-Img List
 let movieImgLoad = document.querySelector('.contents-box');
 
 function movieList(movies) {
@@ -17,7 +23,7 @@ function movieList(movies) {
         let imgBoxDiv = document.createElement('div');
         imgBoxDiv.setAttribute('class', 'img-box');
 
-        imgBoxDiv.innerHTML = `<img src='${movies[i].posterImageFileName}' alt="" />`;
+        imgBoxDiv.innerHTML = `<img src='${movies[i].posterImageFileName}' alt="" id='${i}' />`;
 
         movieImgLoad.appendChild(imgBoxDiv);
     }
@@ -26,8 +32,15 @@ function movieList(movies) {
     let openModalBtn = document.querySelectorAll('.img-box > img');
 
     openModalBtn.forEach((btn) => {
+        // img tag console check
+        // console.log(btn, 'img tag');
+
         btn.addEventListener('click', () => {
-            modalOpenClose();
+            // img id=${i} 값을 변수로 지정
+            let imgId = btn.id;
+
+            // img id 값을 매개변수로 넘겨줌
+            modalOpenClose(imgId);
 
             // 스크롤 제거
             document.body.style.overflow = 'hidden';
@@ -35,71 +48,67 @@ function movieList(movies) {
     });
 }
 
-const modal = `
-<div class='modal-content'>
-    <div id="close-box">
-        <i class="fa-solid fa-xmark close-modal"></i>
-    </div>
-
-    <div class="movieMain-box">
-        <div class="movie-box">
-            <div class="movieTitle-box">
-                <p>유령</p>
-                <p>15세 | 132분</p>
-            </div>
-
-            <div class="movieContents-box">
-                <div class="creator-box">
-                    <p>Director</p>
-                    <p>이해영</p>
-                </div>
-
-                <div class="actor-box">
-                    <p>Actor</p>
-                    <p>설경구, 이하늬, 박소담, 박해수, 서현우</p>
-                </div>
-
-                <div class="summary-box">
-                    <p>
-                                    “유령에게 고함. 작전을 시작한다” 1933년, 일제강점기 경성. 항일조직 ‘흑색단’의 스파이인
-                                    ‘유령’이 비밀리에 활약하고 있다. 새로 부임한 경호대장 카이토는 ‘흑색단’의 총독 암살
-                                    시도를 막기 위해 조선총독부 내의 ‘유령’을 잡으려는 덫을 친다. 영문도 모른 채, ‘유령’으로
-                                    의심받고 벼랑 끝 외딴 호텔에 갇힌 용의자들. 총독부 통신과 감독관 쥰지, 암호문 기록 담당
-                                    차경, 정무총감 비서 유리코, 암호 해독 담당 천계장, 통신과 직원 백호. 이들에게 주어진
-                                    시간은 단 하루 뿐. 기필코 살아나가 동지들을 구하고 총독 암살 작전을 성공시켜야 하는
-                                    ‘유령’과 무사히 집으로 돌아가고 싶은 이들 사이, 의심과 경계는 점점 짙어지는데… 과연
-                                    ‘유령’은 작전에 성공할 수 있을 것인가? “성공할 때까지 멈춰서는 안 된다”
-                    </p>
-                </div>
-            </div>
-        </div>
-                    
-        <div class="detailImg-box">
-            <img src="/assets/img/movie-img/movie_1.jpg" alt="" />
-        </div>
-    </div>
-</div>`;
-
 // Modal Close
-function modalOpenClose() {
-    let div = document.createElement('div');
+// imgId 값을 받아와서 movies[imgId] = json data 배열에서 [imgId]번째 가져오기
+function modalOpenClose(imgId) {
+    movieLoad().then((movies) => {
+        // 제대로 넘어오는지 확인
+        // console.log(num, typeof num, 'modal');
+        let div = document.createElement('div');
 
-    div.setAttribute('id', 'modal');
-    div.innerHTML = modal;
+        div.setAttribute('id', 'modal');
+        div.innerHTML = `
+        <div class='modal-content'>
+            <div id="close-box">
+                <i class="fa-solid fa-xmark close-modal"></i>
+            </div>
 
-    // div body 시작 부분에 삽입
-    document.body.prepend(div);
+            <div class="movieMain-box">
+                <div class="movie-box">
+                    <div class="movieTitle-box">
+                        <span>${movies[imgId].title}</span>
+                        <p>${movies[imgId].permissibleAge}세 | ${movies[imgId].runningTimeMinutes}분</p>
+                    </div>
 
-    let closeModalBtn = document.querySelector('.close-modal');
+                    <div class="movieContents-box">
+                        <div class="creator-box">
+                            <span>Creator</span>
+                            <p>${movies[imgId].creator}</p>
+                        </div>
 
-    closeModalBtn.addEventListener('click', (event) => {
-        // 가장 가까이 있는 #modal 찾기
-        const modal = event.target.closest('#modal');
+                        <div class="actor-box">
+                            <span>Actor</span>
+                            <p>${movies[imgId].castMembers}</p>
+                        </div>
 
-        //모달 요소의 부모 노드에서 모달을 제거
-        modal.parentNode.removeChild(modal);
+                        <div class="summary-box">
+                            <p>
+                            ${movies[imgId].description}
+                            </p>
+                        </div>
+                    </div>
+                </div>
 
-        // 스크롤 등장
-        document.body.style.overflow = 'unset';
+                <div class="detailImg-box">
+                    <img src="${movies[imgId].posterImageFileName}" alt="" />
+                </div>
+            </div>
+        </div>`;
+
+        // div body 시작 부분에 삽입
+        document.body.prepend(div);
+
+        let closeModalBtn = document.querySelector('.close-modal');
+
+        closeModalBtn.addEventListener('click', (event) => {
+            // 가장 가까이 있는 #modal 찾기
+            const modal = event.target.closest('#modal');
+
+            //모달 요소의 부모 노드에서 모달을 제거
+            modal.parentNode.removeChild(modal);
+
+            // 스크롤 등장
+            document.body.style.overflow = 'unset';
+        });
     });
 }
